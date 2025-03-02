99a98fdb06011c46945465b61a9ca198a913bd7a
LucianLucky <glczhlge4071@hotmail.com>
2025-03-02 20:19:47 +0800

    support import/export

--- bin/recipe.js
+++ bin/recipe.js
@@ -2,7 +2,7 @@
 
 const { program } = require('commander');
 const pkg = require('../package.json');
-const { addRecipe, getAllRecipes, searchRecipes } = require('../lib/storage');
+const { addRecipe, getAllRecipes, searchRecipes, exportRecipes, importRecipes } = require('../lib/storage');
 const { formatRecipe, formatRecipeList } = require('../lib/formatter');
 const chalk = require('chalk');
 
@@ -40,6 +40,24 @@ program
   .action((query) => {
     const results = searchRecipes(query);
     console.log(formatRecipeList(results));
+  });
+
+program
+  .command('export <filename>')
+  .description('Export recipes to a JSON file')
+  .action((filename) => {
+    if (exportRecipes(filename)) {
+      console.log(chalk.green(`✓ Recipes exported to ${filename}`));
+    }
+  });
+
+program
+  .command('import <filename>')
+  .description('Import recipes from a JSON file')
+  .action((filename) => {
+    if (importRecipes(filename)) {
+      console.log(chalk.green(`✓ Recipes imported from ${filename}`));
+    }
   });
 
 program.parse(process.argv);
